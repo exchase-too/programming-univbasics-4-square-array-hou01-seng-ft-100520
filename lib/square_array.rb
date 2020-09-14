de64574abe12bf8476c24126ede040f43ecb81c8
Eric Smith <eric.c.smith@sbcglobal.net>
2020-09-14 21:47:21 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,8 +2,8 @@ def square_array(array)
   # your code here
   new_array = []
   counter = 0 
-  while counter < array.length {
+  while counter < array.length do
     new_array.push = array[counter] ** 2 
-  }
+  end
   new_array
 end
