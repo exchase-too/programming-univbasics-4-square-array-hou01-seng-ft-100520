48fb689ff96efd231373119166f56e1361c4146d
Eric Smith <eric.c.smith@sbcglobal.net>
2020-09-14 21:48:29 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -3,7 +3,7 @@ def square_array(array)
   new_array = []
   counter = 0 
   while counter < array.length do
-    new_array.push = array[counter] ** 2 
+    new_array.push(array[counter] ** 2)
   end
   new_array
 end
